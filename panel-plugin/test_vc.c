1c09c664b924e0b240c0cb4b81d9dd4044e5bb43
Danny Milosavljevic <danny_milo@yahoo.com>
2003-07-27 22:08:37 +0000

    ---------------------------------------------------------------------- test program for the vc stuff (oss so far)
    (Old svn revision: 2761)

---
+++ panel-plugin/test_vc.c
@@ -0,0 +1,22 @@
+
+/* compile:
+
+gcc -o test_vc test_vc.c vc.c $(pkg-config --cflags --libs gtk+-2.0) vc_oss.c -DUSE_OSS $(pkg-config --cflags --libs libxfce4util-1.0) -DHAVE_SYS_SOUNDCARD_H -DHAVE_UNISTD_H -DHAVE_SYS_TYPES_H -DHAVE_FCNTL_H
+
+*/
+
+#include <stdio.h>
+#include "vc.h"
+
+extern int register_oss(void);
+
+int main()
+{
+	register_oss ();
+
+	select_vc_direct (*first_vc ());	
+	
+	vc_set_device ("/dev/mixer1");
+
+	vc_set_volume (NULL, 50);
+}
