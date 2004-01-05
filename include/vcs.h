47ac55eae2a99f6003a07879de65dd19fa39d712
Danny Milosavljevic <danny_milo@yahoo.com>
2004-01-05 23:54:16 +0000

    stuffing lowlevel things in a separate library
    (Old svn revision: 2873)

---
+++ include/vcs.h
@@ -0,0 +1,8 @@
+#ifndef __MIXER_VCS_H
+#define __MIXER_VCS_H
+
+int register_vcs ();
+
+#include "vc.h"
+
+#endif
---
+++ lib/vcs.c
@@ -0,0 +1,29 @@
+
+#ifdef USE_ALSA
+extern int register_alsa ();
+#endif
+        
+#ifdef USE_OSS
+extern int register_oss ();
+#endif
+
+#ifdef USE_SUN
+extern int register_sun ();
+#endif
+
+int register_vcs ()
+{
+#ifdef USE_ALSA
+	return register_alsa ();
+#endif
+        
+#ifdef USE_OSS
+	return register_oss ();
+#endif
+
+#ifdef USE_SUN
+	return register_sun ();
+#endif
+
+	return -1;
+}
