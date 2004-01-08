4726a4f708d6408a471338d376fd94864a7bc535
Danny Milosavljevic <danny_milo@yahoo.com>
2004-01-08 17:53:01 +0000

    add missing headers, remove unneccessary cruft
    (Old svn revision: 2902)

---
+++ include/mixer-mcs-names.h
@@ -0,0 +1,6 @@
+#ifndef __MIXER_MCS_NAMES_H
+#define __MIXER_MCS_NAMES_H
+
+#define MIXER_USEFUL_FMT "Sound/Mixer/Useful:%s"
+
+#endif
--- src/mcs_client.h
+++
@@ -1,4 +0,0 @@
-
-void mcs_watch_xfce_channel (void);
-void mcs_stop_watch (void);
-gboolean mcs_mixer_is_useful_control (gchar const *device, gchar const *vcname);
