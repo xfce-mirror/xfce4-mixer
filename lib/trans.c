77ff523213beb81bbfb4577afa4b23ac184380a3
Danny Milosavljevic <danny_milo@yahoo.com>
2004-06-09 21:46:44 +0000

    add missing (dummy) file with translations in it for regeneration of xfce4-mixer.pot to work
    (Old svn revision: 3009)

---
+++ lib/trans.c
@@ -0,0 +1,31 @@
+
+#include <libxfce4util/i18n.h>
+/* these are OSS translateable names */
+
+char *oss_names[] = {
+	_("Vol"), /* Master */
+	_("Pcm"), /* PCM output */
+	_("Spkr"), /* Internal speaker? */
+	_("Line"), /* Line in */
+	_("Mic"), /* Mic in */
+	_("CD"), /* CD in */
+	_("Bass"),
+	_("Trebl"),
+	_("Synth"),
+	_("Mix"),
+	_("Pcm2"),
+	_("Rec"),
+	_("IGain"),
+	_("OGain"),
+	_("Line1"),
+	_("Line2"),
+	_("Line3"),
+	_("Digital1"),
+	_("Digital2"),
+	_("Digital3"),
+	_("PhoneIn"),
+	_("PhoneOut"),
+	_("Video"),
+	_("Radio"),
+	_("Monitor")
+};
