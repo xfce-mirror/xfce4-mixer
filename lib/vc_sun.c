0f70e86ee0924b8a07d2ad55a9a291cc90e2c6ef
Benedikt Meurer <benny@xfce.org>
2004-10-11 18:10:34 +0000

    Here we go, Sun mixer support. Just what I have so far. Seems to block the mixer device, tho it shouldn't. Since I merged this from my test .c files, it may not even compile. I'll check once I get my hands on Solaris next time. Other than that, the code is very straight forward, nothing glorious (just like the solaris audio/mixer system itself :-).
    (Old svn revision: 3118)

--- lib/vc_sun.c
+++ lib/vc_sun.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2004 Danny Milosavljevic <danny_milo@yahoo.com>
- * Copyright (c) 2004 Benedikt Meurer
+ * Copyright (c) 2004 Benedikt Meurer <benny@xfce.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -40,25 +40,31 @@
 #endif
 #include <sys/types.h>
 #include <sys/ioctl.h>
+#include <sys/file.h>
 #include <sys/audioio.h>
+#include <sys/modctl.h>
 #include <unistd.h>
+#include <stropts.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#include <err.h>
+#include <errno.h>
 #include <limits.h>
+#include <string.h>
 
 #define VC_PLUGIN
 #include "vc.h"
 
 #define MAX_MIXERS 10
-#define MIXER_BASE "/dev/mixer"
+#define MIXER_BASE "/dev/audioctl"
 
-static char mixer_device[PATH_MAX] = "/dev/mixer";
+static char mixer_device[PATH_MAX] = MIXER_BASE;
 static int fd = -1;
 
 static int init(void)
 {
+	vc_set_device(dev_name);
+	vc_reinit_device();
 	return USE_THAT;
 }
 
@@ -73,7 +79,7 @@ static void vc_close_device()
 static int vc_reinit_device(void)
 {
 	vc_close_device ();
-	if ((fd = open(mixer_device, O_RDONLY | O_SHLOCK)) < 0)
+	if ((fd = open(mixer_device, O_RDONLY)) < 0)
 		return -1;
 		
 	return 0;
@@ -81,45 +87,192 @@ static int vc_reinit_device(void)
 
 static void vc_set_device(char const *name)
 {
-	if (!name)
-		mixer_device[0] = 0;
-	else
-		(void)strlcpy(mixer_device, name, PATH_MAX);
+  /* Not supported */
 }
 
 
+static gboolean set_dev (const char *which, int *pport, gboolean *pinput)
+{
+  gboolean input = FALSE;
+  gint port = 0;
+  audio_info_t info;
+
+  if (strcmp (which, "Headphone") == 0)
+    {
+      input = FALSE;
+      port = AUDIO_HEADPHONE;
+    }
+  else if (strcmp (which, "Speaker") == 0)
+    {
+      input = FALSE;
+      port = AUDIO_SPEAKER;
+    }
+  else if (strcmp (which, "Line Out") == 0)
+    {
+      input = FALSE;
+      port = AUDIO_LINE_OUT;
+    }
+  else if (strcmp (which, "Microphone") == 0)
+    {
+      input = TRUE;
+      port = AUDIO_MICROPHONE;
+    }
+  else if (strcmp (which, "Line In") == 0)
+    {
+      input = TRUE;
+      port = AUDIO_LINE_IN;
+    }
+  else if (strcmp (which, "CD") == 0)
+    {
+      input = TRUE;
+      port = AUDIO_CD;
+    }
+  else if (strcmp (which, "SPDIF In") == 0)
+    {
+      input = TRUE;
+      port = AUDIO_SPDIF_IN;
+    }
+  else if (strcmp (which, "Aux1 In") == 0)
+    {
+      input = TRUE;
+      port = AUDIO_AUX1_IN;
+    }
+  else if (strcmp (which, "Aux2 In") == 0)
+    {
+      input = TRUE;
+      port = AUDIO_AUX2_IN;
+    }
+  else
+    {
+      return FALSE;
+    }
+
+  AUDIO_INITINFO (&info);
+
+  if (ioctl (fd, AUDIO_GETINFO, &info) < 0)
+    {
+      g_warning ("Unable to query audio info from %s: %s", MIXER_BASE, g_strerror (errno));
+      close (fd);
+      fd = -1;
+      return FALSE;
+    }
+
+  if (input)
+    {
+      if (info.record.port == port)
+        return TRUE;
+      info.record.port = port;
+    }
+  else
+    {
+      if (info.play.port == port)
+        return TRUE;
+      info.play.port = port;
+    }
+
+  if (ioctl (fd, AUDIO_SETINFO, &info) < 0)
+    {
+      g_warning ("Unable to set audio info on %s: %s", MIXER_BASE, g_strerror (errno));
+      close (fd);
+      fd = -1;
+      return FALSE;
+    }
+
+  *pport = port;
+  *pinput = input;
+
+  return TRUE;
+}
+
 static int vc_get_volume(char const *which)
 {
-	/*return: 0..100*/
-	/*someone with netbsd fill that in please. can't be that hard*/
-	return 0;  
+  gboolean input;
+  gint port;
+  audio_info_t info;
+  gint gain;
+
+  if (fd < 0)
+    return 0;
+
+  if (!set_dev (which, &port, &input))
+    return 0;
+
+  AUDIO_INITINFO (&info);
+
+  if (ioctl (fd, AUDIO_GETINFO, &info) < 0)
+    {
+      g_warning ("Unable to query volume from %s: %s", MIXER_BASE, g_strerror (errno));
+      close (fd);
+      fd = -1;
+      return 0;
+    }
+
+  if (input)
+    gain = info.record.gain;
+  else
+    gain = info.play.gain;
+
+  gain = ((gain - AUDIO_MIN_GAIN) * 100) / (AUDIO_MAX_GAIN - AUDIO_MIN_GAIN);
+
+	return gain;
 }
 
 static void vc_set_volume(char const *which, int vol_p)
 {
-	/*vol_p: 0..100 */
-	/*someone with netbsd fill that in please. can't be that hard*/
+  gboolean input;
+  gint port;
+  audio_info_t info;
+  gint gain;
+
+  if (fd < 0)
+    return;
+
+  if (!set_dev (which, &port, &input))
+    return;
+
+  AUDIO_INITINFO (&info);
+
+  if (ioctl (fd, AUDIO_GETINFO, &info) < 0)
+    {
+      g_warning ("Unable to query volume from %s: %s", MIXER_BASE, g_strerror (errno));
+      close (fd);
+      fd = -1;
+      return;
+    }
+
+  gain = vol_p * (AUDIO_MAX_GAIN - AUDIO_MIN_GAIN) / 100;
+  if (input)
+    info.record.gain = gain;
+  else
+    info.play.gain = gain;
+
+  if (ioctl (fd, AUDIO_SETINFO, &info) < 0)
+    {
+      g_warning ("Unable to set volumne of %s: %s", MIXER_BASE, g_strerror (errno));
+      close (fd);
+      fd = -1;
+      return;
+    }
 }
 
 static GList *vc_get_control_list()
 {
-#if 0
-GList *g;
-g = NULL;
+  GList *lp = NULL;
 
-for each control do {
-	volcontrol_t *c;
-	c = g_new0(volcontrol_t, 1);
-	c->name = g_strdup_printf("%s,%u", n, id); /* unique name, no other naming rules except it should look pretty */
-	c->type = CT_SLIDER;
-	g = g_list_append(g, c);
-}
-return g;
+  /* output ports */
+  lp = g_list_append (lp, g_strdup ("Headphone"));
+  lp = g_list_append (lp, g_strdup ("Speaker"));
+  lp = g_list_append (lp, g_strdup ("Line Out"));
 
-someone with netbsd fill that in please
-#endif
+  /* input ports */
+  lp = g_list_append (lp, g_strdup ("Microphone"));
+  lp = g_list_append (lp, g_strdup ("Line In"));
+  lp = g_list_append (lp, g_strdup ("CD"));
+  lp = g_list_append (lp, g_strdup ("SPDIF In"));
+  lp = g_list_append (lp, g_strdup ("Aux1 In"));
+  lp = g_list_append (lp, g_strdup ("Aux2 In"));
 
-	return NULL;
+	return lp;
 }
 
 static void vc_set_volume_callback(volchanger_callback_t cb, void *data)
@@ -130,33 +283,7 @@ static void vc_set_volume_callback(volchanger_callback_t cb, void *data)
 
 static GList *vc_get_device_list()
 {
-	int i;
-	int fd;
-	GList *g;
-	char device[PATH_MAX];
-	audio_device_t devinfo;
-
-	g = NULL;
-	for(i = 0; i < MAX_MIXERS; i++) {
-		(void)snprintf(device, PATH_MAX, "%s%d", MIXER_BASE, n);
-
-		if ((fd = open(device, O_RDONLY | O_SHLOCK)) < 0)
-			continue;
-
-		if (ioctl(fd, AUDIO_GETDEV, &devinfo) < 0) {
-			(void)close(fd);
-			continue;
-		}
-		
-		/*
-		   (void)strlcpy(mixers[n].name, devinfo.name, MAX_AUDIO_DEV_LEN);      #
-	    (void)strlcpy(mixers[n].config, devinfo.config,                              MAX_AUDIO_DEV_LEN);
-		*/
-		
-		(void)close(fd);
-		g = g_list_append (g, g_strdup (device));
-	}
-	return g;
+  return g_list_append (NULL, g_strdup (MIXER_BASE));
 }
 
 static void vc_set_select(char const *which, gchar const *v)
@@ -179,7 +306,7 @@ static gboolean vc_get_switch(char const *which)
 
 static char const *vc_get_device()
 {
-	return mixer_device;
+	return NULL;
 }
 
 static void vc_handle_events()
