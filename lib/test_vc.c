084fc4b3548d59073913f16ef280ed8fdafac1a6
Danny Milosavljevic <danny_milo@yahoo.com>
2005-12-31 01:34:19 +0000

    add control test
    (Old svn revision: 19213)

--- lib/test_vc.c
+++ lib/test_vc.c
@@ -8,6 +8,9 @@ int main()
 {
   GList* devices;
   GList* device_item;
+  GList* controls;
+  GList* control_item;
+  volcontrol_t* control1;
   gchar const* device_name;
   register_vcs ();
   
@@ -18,6 +21,19 @@ int main()
       device_name = (gchar const*) device_item->data;
       
       printf(" Device: %s\n", device_name);
+      
+      vc_set_device (device_name);
+      
+      controls = vc_get_control_list ();
+      control_item = controls;
+      while (control_item != NULL) {
+        control1 = (volcontrol_t*) control_item->data;
+        
+        printf("   Control: %s\n", control1->name);
+        
+        control_item = g_list_next (control_item);
+      }
+      
       
       device_item = g_list_next (device_item);
     }
