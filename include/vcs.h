969b85e5cc038a4b04226cc30d8e9e97945548b2
Danny Milosavljevic <danny_milo@yahoo.com>
2009-01-11 19:37:33 +0000

    fix recurring 'handle_events'.
    (Old svn revision: 29173)

--- include/vcs.h
+++ include/vcs.h
@@ -4,6 +4,11 @@
 /* call this at the beginning of your program. */
 int register_vcs ();
 
+/* Optionally register a event callback (per device) with
+     vc_set_volume_callback(callback, user_data);
+   call vc_handle_events() periodically.
+ */
+
 #include "vc.h"
 
 #endif
