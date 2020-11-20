01b3e9b71f45715e62a9c4f8076481fd507d57d2
Landry Breuil <landry@openbsd.org>
2020-11-20 18:33:16 +0100

    first baby steps calling sioctl_open and ondesc callback

--- libxfce4mixer/sndio/sndio-mixer.c
+++ libxfce4mixer/sndio/sndio-mixer.c
@@ -28,18 +28,58 @@
 #include "sndio-track.h"
 #include "sndio-options.h"
 
+/* globals */
 struct _GstMixerSndio
 {
   GstMixer parent;
-
+  struct sioctl_hdl *hdl;
 };
 
 G_DEFINE_TYPE (GstMixerSndio, gst_mixer_sndio, GST_TYPE_MIXER)
+
+/* sndio callbacks */
+void
+onctl(void *arg, unsigned addr, unsigned val)
+{
+  GstMixerSndio *mixer = GST_MIXER_SNDIO (arg);
+  mixer = NULL;
+  g_warning("onctl called: addr=%d, val=%d", addr, val);
+}
+
+/* callback called when a description changes - e.g when a new track is added/removed */
+void
+ondesc(void *arg, struct sioctl_desc *d, int curval)
+{
+  GstMixerSndio *mixer = GST_MIXER_SNDIO (arg);
+  mixer = NULL;
+  if (d == NULL) {
+    g_warning("got the full set of track descriptions");
+    return;
+  }
+  g_warning("ondesc called: %s/%s.%s=%d (max=%d)", d->group, d->node0.name, d->func, curval, d->maxval);
+/* create a gst_track_sndio by distinct desc (desc is appname, input, output..)
+group is null or app
+func is level or mute
+
+ignore server.device for now
+
+input.level=0.486
+input.mute=0
+output.level=1.000
+output.mute=0
+server.device=0
+app/mplayer0.level=1.000
+
+-> 3 tracks, input has GST_MIXER_TRACK_INPUT flag, all others have GST_MIXER_TRACK_OUTPUT flag
+input and output have a real mute control, appname doesnt (eg set volume to 0)
+*/
+}
 
 static void
 gst_mixer_sndio_finalize (GObject *self)
 {
   GstMixerSndio *mixer = GST_MIXER_SNDIO (self);
+  mixer = NULL;
 
   G_OBJECT_CLASS (gst_mixer_sndio_parent_class)->finalize (self);
 }
@@ -60,6 +100,7 @@ gst_mixer_sndio_get_mixer_flags (GstMixer *mixer)
 static void gst_mixer_sndio_set_volume (GstMixer *mixer, GstMixerTrack *track, gint *volumes)
 {
   GstMixerSndio *sndio = GST_MIXER_SNDIO (mixer);
+  sndio = NULL;
   int i;
 
   for (i = 0; i < NUM_CHANNELS(track); i++)
@@ -82,6 +123,7 @@ static void
 gst_mixer_sndio_set_record (GstMixer * mixer, GstMixerTrack *track, gboolean record)
 {
   GstMixerSndio *sndio = GST_MIXER_SNDIO (mixer);
+  sndio = NULL;
 }
 
 
@@ -89,6 +131,7 @@ static void
 gst_mixer_sndio_set_mute (GstMixer *mixer, GstMixerTrack *track, gboolean mute)
 {
   GstMixerSndio *sndio = GST_MIXER_SNDIO (mixer);
+  sndio = NULL;
 }
 
 
@@ -128,25 +171,40 @@ gst_mixer_sndio_class_init (GstMixerSndioClass *klass)
   object_class->finalize = (void (*) (GObject *object)) gst_mixer_sndio_finalize;
 }
 
-static int
-gst_mixer_sndio_new (GstMixer **mixer_ret)
+GstMixer*
+gst_mixer_sndio_new (struct sioctl_hdl *hdl)
 {
   GstMixerSndio *sndio;
+  char *devname = SIO_DEVANY;
 
   sndio = g_object_new (GST_MIXER_TYPE_SNDIO,
                         "name", "sndio",
                         "card-name", g_strdup (_("Sndio Volume Control")),
                         NULL);
+   sndio->hdl = hdl;
+   if (!sioctl_ondesc(hdl, ondesc, sndio)) {
+     g_critical("%s: can't get device description", devname);
+     return NULL;
+   }
+
+   sioctl_onval(hdl, onctl, sndio);
+   return GST_MIXER(sndio);
 }
 
 GList *gst_mixer_sndio_probe (GList *card_list)
 {
   GstMixer *mixer = NULL;
-  int ret;
+  struct sioctl_hdl *hdl;
+  char *devname = SIO_DEVANY;
+  hdl = sioctl_open(devname, SIOCTL_READ | SIOCTL_WRITE, 0);
+  if (hdl == NULL) {
+    g_critical ("Failed to open device '%s'", devname);
+    return NULL;
+  }
 
-  ret = gst_mixer_sndio_new(&mixer);
+  mixer = gst_mixer_sndio_new(hdl);
 
-  if (ret < 0)
+  if (mixer == NULL)
     return NULL;
 
   card_list = g_list_append (card_list, mixer);
