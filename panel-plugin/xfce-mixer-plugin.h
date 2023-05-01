xfce-mirror/xfce4-mixer

commit 11ade74fa75ed7566cd91ff67810235e65f058df

panel-plugin: Use G_DECLARE_FINAL_TYPE
Avoids boilerplate code and typos/naming inconsistencies by the way.

panel-plugin/xfce-mixer-plugin.h
@@ -23,19 +23,12 @@
 
 #include <gtk/gtk.h>
 
+#include <libxfce4panel/libxfce4panel.h>
+
 G_BEGIN_DECLS
 
-typedef struct _XfceMixerPluginClass XfceMixerPluginClass;
-typedef struct _XfceMixerPlugin      XfceMixerPlugin;
-
-#define TYPE_XFCE_MIXER_PLUGIN            (xfce_mixer_plugin_get_type ())
-#define XFCE_MIXER_PLUGIN(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), TYPE_XFCE_MIXER_PLUGIN, XfceMixerPlugin))
-#define XFCE_MIXER_PLUGIN_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), TYPE_XFCE_MIXER_PLUGIN, XfceMixerPluginClass))
-#define IS_XFCE_MIXER_PLUGIN(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), TYPE_XFCE_MIXER_PLUGIN))
-#define IS_XFCE_MIXER_PLUGIN_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), TYPE_XFCE_MIXER_PLUGIN))
-#define XFCE_MIXER_PLUGIN_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), TYPE_XFCE_MIXER_PLUGIN, XfceMixerPluginClass))
-
-GType     xfce_mixer_plugin_get_type       (void) G_GNUC_CONST;
+#define XFCE_TYPE_MIXER_PLUGIN (xfce_mixer_plugin_get_type ())
+G_DECLARE_FINAL_TYPE (XfceMixerPlugin, xfce_mixer_plugin, XFCE, MIXER_PLUGIN, XfcePanelPlugin)
 
 void  xfce_mixer_plugin_register_type (XfcePanelTypeModule *type_module);
 

panel-plugin/xfce-mixer-plugin.c
@@ -33,7 +33,6 @@
 
 #include <libxfce4util/libxfce4util.h>
 #include <libxfce4ui/libxfce4ui.h>
-#include <libxfce4panel/libxfce4panel.h>
 #include <xfconf/xfconf.h>
 
 #ifdef HAVE_KEYBINDER
@@ -120,14 +119,6 @@ static void     xfce_mixer_plugin_mute_pressed            (const char         *k
                                                            void               *user_data);
 #endif
 
-
-
-
-struct _XfceMixerPluginClass
-{
-  /* Parent class */
-  XfcePanelPluginClass __parent__;
-};
 
 
 
@@ -637,7 +628,7 @@ xfce_mixer_plugin_screen_position_changed (XfcePanelPlugin    *plugin,
 {
   XfceMixerPlugin *mixer_plugin = XFCE_MIXER_PLUGIN (plugin);
 
-  g_return_if_fail (IS_XFCE_MIXER_PLUGIN (mixer_plugin));
+  g_return_if_fail (XFCE_IS_MIXER_PLUGIN (mixer_plugin));
   g_return_if_fail (GTK_IS_WIDGET (mixer_plugin->button));
 
   xfce_volume_button_set_screen_position (XFCE_VOLUME_BUTTON (mixer_plugin->button), screen_position);
@@ -665,7 +656,7 @@ xfce_mixer_plugin_get_volume (XfceMixerPlugin *mixer_plugin)
   gint *volumes;
   gint  volume;
 
-  g_return_val_if_fail (IS_XFCE_MIXER_PLUGIN (mixer_plugin), 0);
+  g_return_val_if_fail (XFCE_IS_MIXER_PLUGIN (mixer_plugin), 0);
   g_return_val_if_fail (GST_IS_MIXER (mixer_plugin->card), 0);
   g_return_val_if_fail (GST_IS_MIXER_TRACK (mixer_plugin->track), 0);
 
@@ -688,7 +679,7 @@ xfce_mixer_plugin_set_volume (XfceMixerPlugin *mixer_plugin,
   gint *volumes;
   gint  i;
 
-  g_return_if_fail (IS_XFCE_MIXER_PLUGIN (mixer_plugin));
+  g_return_if_fail (XFCE_IS_MIXER_PLUGIN (mixer_plugin));
   g_return_if_fail (GST_IS_MIXER (mixer_plugin->card));
   g_return_if_fail (GST_IS_MIXER_TRACK (mixer_plugin->track));
 
@@ -720,7 +711,7 @@ xfce_mixer_plugin_get_muted (XfceMixerPlugin *mixer_plugin)
   XfceMixerTrackType track_type;
   gboolean           muted = FALSE;
 
-  g_return_val_if_fail (IS_XFCE_MIXER_PLUGIN (mixer_plugin), FALSE);
+  g_return_val_if_fail (XFCE_IS_MIXER_PLUGIN (mixer_plugin), FALSE);
   g_return_val_if_fail (GST_IS_MIXER (mixer_plugin->card), FALSE);
   g_return_val_if_fail (GST_IS_MIXER_TRACK (mixer_plugin->track), FALSE);
 
@@ -742,7 +733,7 @@ xfce_mixer_plugin_set_muted (XfceMixerPlugin *mixer_plugin,
 {
   XfceMixerTrackType track_type;
 
-  g_return_if_fail (IS_XFCE_MIXER_PLUGIN (mixer_plugin));
+  g_return_if_fail (XFCE_IS_MIXER_PLUGIN (mixer_plugin));
   g_return_if_fail (GST_IS_MIXER (mixer_plugin->card));
   g_return_if_fail (GST_IS_MIXER_TRACK (mixer_plugin->track));
 
@@ -785,7 +776,7 @@ xfce_mixer_plugin_update_volume (XfceMixerPlugin *mixer_plugin,
 {
   gdouble button_volume;
 
-  g_return_if_fail (IS_XFCE_MIXER_PLUGIN (mixer_plugin));
+  g_return_if_fail (XFCE_IS_MIXER_PLUGIN (mixer_plugin));
   g_return_if_fail (GST_IS_MIXER (mixer_plugin->card));
   g_return_if_fail (GST_IS_MIXER_TRACK (mixer_plugin->track));
 
@@ -804,7 +795,7 @@ static void
 xfce_mixer_plugin_update_muted (XfceMixerPlugin *mixer_plugin,
                                 gboolean         muted)
 {
-  g_return_if_fail (IS_XFCE_MIXER_PLUGIN (mixer_plugin));
+  g_return_if_fail (XFCE_IS_MIXER_PLUGIN (mixer_plugin));
 
   /* Update the button */
   g_signal_handlers_block_by_func (G_OBJECT (mixer_plugin->button), xfce_mixer_plugin_button_is_muted, mixer_plugin);
@@ -825,7 +816,7 @@ xfce_mixer_plugin_update_track (XfceMixerPlugin *mixer_plugin)
   XfceMixerTrackType track_type;
   gboolean           muted;
 
-  g_return_if_fail (IS_XFCE_MIXER_PLUGIN (mixer_plugin));
+  g_return_if_fail (XFCE_IS_MIXER_PLUGIN (mixer_plugin));
 
   /* Set the volume button to invalid state and return if the card or track is invalid */
   if (!GST_IS_MIXER (mixer_plugin->card) || !GST_IS_MIXER_TRACK (mixer_plugin->track))

panel-plugin/xfce-plugin-dialog.c
@@ -27,7 +27,6 @@
 
 #include <gst/gst.h>
 
-#include <libxfce4ui/libxfce4ui.h>
 #include <libxfce4panel/libxfce4panel.h>
 #include <xfconf/xfconf.h>
 
@@ -54,13 +53,6 @@ static void xfce_plugin_dialog_soundcard_property_changed (XfcePluginDialog     
 static void xfce_plugin_dialog_track_property_changed     (XfcePluginDialog      *dialog,
                                                            GParamSpec            *pspec,
                                                            GObject               *object);
-
-
-
-struct _XfcePluginDialogClass
-{
-  XfceTitledDialogClass __parent__;
-};
 
 
 
@@ -128,7 +120,7 @@ xfce_plugin_dialog_new (XfcePanelPlugin *plugin)
 {
   XfcePluginDialog *dialog;
 
-  dialog = XFCE_PLUGIN_DIALOG (g_object_new (TYPE_XFCE_PLUGIN_DIALOG, NULL));
+  dialog = XFCE_PLUGIN_DIALOG (g_object_new (XFCE_TYPE_PLUGIN_DIALOG, NULL));
   dialog->plugin = XFCE_MIXER_PLUGIN (plugin);
 
   xfce_plugin_dialog_create_contents (dialog);
@@ -240,7 +232,7 @@ xfce_plugin_dialog_soundcard_property_changed (XfcePluginDialog *dialog,
   gchar       *new_card_name;
   const gchar *old_card_name = NULL;
 
-  g_return_if_fail (IS_XFCE_PLUGIN_DIALOG (dialog));
+  g_return_if_fail (XFCE_IS_PLUGIN_DIALOG (dialog));
   g_return_if_fail (G_IS_OBJECT (object));
   g_return_if_fail (IS_XFCE_MIXER_CARD_COMBO (dialog->card_combo));
   g_return_if_fail (IS_XFCE_MIXER_TRACK_COMBO (dialog->track_combo));
@@ -280,7 +272,7 @@ xfce_plugin_dialog_track_property_changed (XfcePluginDialog *dialog,
   const gchar   *old_track_label = NULL;
   gchar         *new_track_label;
 
-  g_return_if_fail (IS_XFCE_PLUGIN_DIALOG (dialog));
+  g_return_if_fail (XFCE_IS_PLUGIN_DIALOG (dialog));
   g_return_if_fail (G_IS_OBJECT (object));
   g_return_if_fail (IS_XFCE_MIXER_CARD_COMBO (dialog->card_combo));
   g_return_if_fail (IS_XFCE_MIXER_TRACK_COMBO (dialog->track_combo));

panel-plugin/xfce-plugin-dialog.h
@@ -23,22 +23,17 @@
 #define __XFCE_PLUGIN_DIALOG_H__
 
 #include <gtk/gtk.h>
-
 #include <gst/gst.h>
+
+#include <libxfce4ui/libxfce4ui.h>
 
 G_BEGIN_DECLS
 
-typedef struct _XfcePluginDialogClass XfcePluginDialogClass;
-typedef struct _XfcePluginDialog      XfcePluginDialog;
-
-#define TYPE_XFCE_PLUGIN_DIALOG            (xfce_plugin_dialog_get_type ())
-#define XFCE_PLUGIN_DIALOG(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), TYPE_XFCE_PLUGIN_DIALOG, XfcePluginDialog))
-#define XFCE_PLUGIN_DIALOG_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), TYPE_XFCE_PLUGIN_DIALOG, XfcePluginDialogClass))
-#define IS_XFCE_PLUGIN_DIALOG(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), TYPE_XFCE_PLUGIN_DIALOG))
-#define IS_XFCE_PLUGIN_DIALOG_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), TYPE_XFCE_PLUGIN_DIALOG))
-#define XFCE_PLUGIN_DIALOG_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), TYPE_XFCE_PLUGIN_DIALOG, XfcePluginDialogClass))
-
-GType     xfce_plugin_dialog_get_type  (void) G_GNUC_CONST;
+#define XFCE_TYPE_PLUGIN_DIALOG (xfce_plugin_dialog_get_type ())
+#ifndef glib_autoptr_clear_XfceTitledDialog
+G_DEFINE_AUTOPTR_CLEANUP_FUNC (XfceTitledDialog, g_object_unref)
+#endif
+G_DECLARE_FINAL_TYPE (XfcePluginDialog, xfce_plugin_dialog, XFCE, PLUGIN_DIALOG, XfceTitledDialog)
 
 GtkWidget *xfce_plugin_dialog_new (XfcePanelPlugin *plugin);
 

panel-plugin/xfce-volume-button.c
@@ -105,8 +105,6 @@ static gboolean   xfce_volume_button_button_press_event   (GtkWidget            
                                                            GdkEventButton        *event);
 static gboolean   xfce_volume_button_scroll_event         (GtkWidget             *widget,
                                                            GdkEventScroll        *event);
-static void       xfce_volume_button_volume_changed       (XfceVolumeButton      *button,
-                                                           gdouble                volume);
 static void       xfce_volume_button_update_icons         (XfceVolumeButton      *button,
                                                            GtkIconTheme          *icon_theme);
 static void       xfce_volume_button_toggled              (GtkToggleButton       *toggle_button);
@@ -124,15 +122,6 @@ static gboolean   xfce_volume_button_dock_grab_broken     (XfceVolumeButton     
                                                            GtkWidget             *widget);
 
 
-
-struct _XfceVolumeButtonClass
-{
-  GtkToggleButtonClass __parent__;
-
-  /* Signals */
-  void (*volume_changed) (XfceVolumeButton *button,
-                          gdouble           volume);
-};
 
 struct _XfceVolumeButton
 {
@@ -199,8 +188,6 @@ xfce_volume_button_class_init (XfceVolumeButtonClass *klass)
   gtk_toggle_button_class = GTK_TOGGLE_BUTTON_CLASS (klass);
   gtk_toggle_button_class->toggled = xfce_volume_button_toggled;
 
-  klass->volume_changed = xfce_volume_button_volume_changed;
-
   g_object_class_install_property (gobject_class,
                                    PROP_TRACK_LABEL,
                                    g_param_spec_string ("track-label",
@@ -246,7 +233,7 @@ xfce_volume_button_class_init (XfceVolumeButtonClass *klass)
   button_signals[VOLUME_CHANGED] = g_signal_new ("volume-changed",
                                                  G_TYPE_FROM_CLASS (klass),
                                                  G_SIGNAL_RUN_LAST | G_SIGNAL_ACTION,
-                                                 G_STRUCT_OFFSET (XfceVolumeButtonClass, volume_changed),
+                                                 0,
                                                  NULL, 
                                                  NULL,
                                                  g_cclosure_marshal_VOID__DOUBLE,
@@ -442,7 +429,7 @@ static void xfce_volume_button_get_property (GObject      *object,
 GtkWidget*
 xfce_volume_button_new (void)
 {
-  return g_object_new (TYPE_XFCE_VOLUME_BUTTON, NULL);
+  return g_object_new (XFCE_TYPE_VOLUME_BUTTON, NULL);
 }
 
 
@@ -860,7 +847,7 @@ xfce_volume_button_update (XfceVolumeButton *button)
   guint      i;
   gchar     *tip_text;
 
-  g_return_if_fail (IS_XFCE_VOLUME_BUTTON (button));
+  g_return_if_fail (XFCE_IS_VOLUME_BUTTON (button));
 
   /* Get upper, lower bounds and current value of the adjustment */
   g_object_get (G_OBJECT (button->adjustment), "upper", &upper, "lower", &lower, "value", &value, NULL);
@@ -906,21 +893,12 @@ xfce_volume_button_update (XfceVolumeButton *button)
 
 
 static void
-xfce_volume_button_volume_changed (XfceVolumeButton *button,
-                                   gdouble           volume)
-{
-  /* Do nothing */
-}
-
-
-
-static void
 xfce_volume_button_update_icons (XfceVolumeButton *button,
                                  GtkIconTheme     *icon_theme)
 {
   guint i;
 
-  g_return_if_fail (IS_XFCE_VOLUME_BUTTON (button));
+  g_return_if_fail (XFCE_IS_VOLUME_BUTTON (button));
   g_return_if_fail (GTK_IS_ICON_THEME (icon_theme));
 
   /* Pre-load all icons */
@@ -1024,7 +1002,7 @@ xfce_volume_button_set_no_mute (XfceVolumeButton *button,
 {
   GValue value = { 0 };
 
-  g_return_if_fail (IS_XFCE_VOLUME_BUTTON (button));
+  g_return_if_fail (XFCE_IS_VOLUME_BUTTON (button));
 
   g_value_init (&value, G_TYPE_BOOLEAN);
   g_value_set_boolean (&value, no_mute);
@@ -1038,7 +1016,7 @@ xfce_volume_button_get_no_mute (XfceVolumeButton *button)
 {
   GValue value = { 0 };
 
-  g_return_val_if_fail (IS_XFCE_VOLUME_BUTTON (button), FALSE);
+  g_return_val_if_fail (XFCE_IS_VOLUME_BUTTON (button), FALSE);
 
   g_value_init (&value, G_TYPE_BOOLEAN);
   g_object_get_property (G_OBJECT (button), "no-mute", &value);
@@ -1054,7 +1032,7 @@ xfce_volume_button_set_muted (XfceVolumeButton *button,
 {
   GValue value = { 0 };
 
-  g_return_if_fail (IS_XFCE_VOLUME_BUTTON (button));
+  g_return_if_fail (XFCE_IS_VOLUME_BUTTON (button));
 
   g_value_init (&value, G_TYPE_BOOLEAN);
   g_value_set_boolean (&value, is_muted);
@@ -1068,7 +1046,7 @@ xfce_volume_button_get_muted (XfceVolumeButton *button)
 {
   GValue value = { 0 };
 
-  g_return_val_if_fail (IS_XFCE_VOLUME_BUTTON (button), FALSE);
+  g_return_val_if_fail (XFCE_IS_VOLUME_BUTTON (button), FALSE);
 
   g_value_init (&value, G_TYPE_BOOLEAN);
   g_object_get_property (G_OBJECT (button), "is-muted", &value);
@@ -1082,7 +1060,7 @@ void
 xfce_volume_button_set_volume (XfceVolumeButton *button,
                                gdouble           volume)
 {
-  g_return_if_fail (IS_XFCE_VOLUME_BUTTON (button));
+  g_return_if_fail (XFCE_IS_VOLUME_BUTTON (button));
 
   /* Set the value of the adjustment */
   gtk_adjustment_set_value (GTK_ADJUSTMENT (button->adjustment), volume);
@@ -1097,7 +1075,7 @@ void
 xfce_volume_button_set_icon_size (XfceVolumeButton *button,
                                   gint              size)
 {
-  g_return_if_fail (IS_XFCE_VOLUME_BUTTON (button));
+  g_return_if_fail (XFCE_IS_VOLUME_BUTTON (button));
   g_return_if_fail (size >= 0);
 
   /* Remember the icon size */
@@ -1114,7 +1092,7 @@ xfce_volume_button_set_track_label (XfceVolumeButton *button,
 {
   GValue value = { 0 };
 
-  g_return_if_fail (IS_XFCE_VOLUME_BUTTON (button));
+  g_return_if_fail (XFCE_IS_VOLUME_BUTTON (button));
 
   g_value_init (&value, G_TYPE_STRING);
   g_value_set_string (&value, track_label);
@@ -1128,7 +1106,7 @@ xfce_volume_button_get_track_label (XfceVolumeButton *button)
 {
   GValue value = { 0 };
 
-  g_return_val_if_fail (IS_XFCE_VOLUME_BUTTON (button), NULL);
+  g_return_val_if_fail (XFCE_IS_VOLUME_BUTTON (button), NULL);
 
   g_value_init (&value, G_TYPE_STRING);
   g_object_get_property (G_OBJECT (button), "track-label", &value);
@@ -1144,7 +1122,7 @@ xfce_volume_button_set_is_configured (XfceVolumeButton *button,
 {
   GValue value = { 0 };
 
-  g_return_if_fail (IS_XFCE_VOLUME_BUTTON (button));
+  g_return_if_fail (XFCE_IS_VOLUME_BUTTON (button));
 
   g_value_init (&value, G_TYPE_BOOLEAN);
   g_value_set_boolean (&value, is_configured);
@@ -1158,7 +1136,7 @@ xfce_volume_button_get_is_configured (XfceVolumeButton *button)
 {
   GValue value = { 0 };
 
-  g_return_val_if_fail (IS_XFCE_VOLUME_BUTTON (button), FALSE);
+  g_return_val_if_fail (XFCE_IS_VOLUME_BUTTON (button), FALSE);
 
   g_value_init (&value, G_TYPE_BOOLEAN);
   g_object_get_property (G_OBJECT (button), "is-configured", &value);
@@ -1174,7 +1152,7 @@ xfce_volume_button_set_screen_position (XfceVolumeButton   *button,
 {
   GValue value = { 0 };
 
-  g_return_if_fail (IS_XFCE_VOLUME_BUTTON (button));
+  g_return_if_fail (XFCE_IS_VOLUME_BUTTON (button));
 
   g_value_init (&value, XFCE_TYPE_SCREEN_POSITION);
   g_value_set_enum (&value, screen_position);
@@ -1188,7 +1166,7 @@ xfce_volume_button_get_screen_position (XfceVolumeButton *button)
 {
   GValue value = { 0 };
 
-  g_return_val_if_fail (IS_XFCE_VOLUME_BUTTON (button), FALSE);
+  g_return_val_if_fail (XFCE_IS_VOLUME_BUTTON (button), FALSE);
 
   g_value_init (&value, XFCE_TYPE_SCREEN_POSITION);
   g_object_get_property (G_OBJECT (button), "screen-position", &value);

panel-plugin/xfce-volume-button.h
@@ -28,17 +28,8 @@
 
 G_BEGIN_DECLS
 
-typedef struct _XfceVolumeButtonClass XfceVolumeButtonClass;
-typedef struct _XfceVolumeButton      XfceVolumeButton;
-
-#define TYPE_XFCE_VOLUME_BUTTON            (xfce_volume_button_get_type ())
-#define XFCE_VOLUME_BUTTON(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), TYPE_XFCE_VOLUME_BUTTON, XfceVolumeButton))
-#define XFCE_VOLUME_BUTTON_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), TYPE_XFCE_VOLUME_BUTTON, XfceVolumeButtonClass))
-#define IS_XFCE_VOLUME_BUTTON(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), TYPE_XFCE_VOLUME_BUTTON))
-#define IS_XFCE_VOLUME_BUTTON_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), TYPE_XFCE_VOLUME_BUTTON))
-#define XFCE_VOLUME_BUTTON_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), TYPE_XFCE_VOLUME_BUTTON, XfceVolumeButtonClass))
-
-GType     xfce_volume_button_get_type           (void) G_GNUC_CONST;
+#define XFCE_TYPE_VOLUME_BUTTON (xfce_volume_button_get_type ())
+G_DECLARE_FINAL_TYPE (XfceVolumeButton, xfce_volume_button, XFCE, VOLUME_BUTTON, GtkToggleButton)
 
 GtkWidget *        xfce_volume_button_new                 (void);
 void               xfce_volume_button_set_no_mute         (XfceVolumeButton   *button,

commit 4deb2d63a1f4fec8c5360dffb7cabf137c6c0ff8

panel-plugin: Define glib_autoptr_clear_XfcePanelPlugin if needed
Fixes: 11ade74fa75ed7566cd91ff67810235e65f058df
Related: !6

panel-plugin/xfce-mixer-plugin.h
@@ -28,6 +28,9 @@
 G_BEGIN_DECLS
 
 #define XFCE_TYPE_MIXER_PLUGIN (xfce_mixer_plugin_get_type ())
+#if !LIBXFCE4PANEL_CHECK_VERSION (4, 19, 1)
+G_DEFINE_AUTOPTR_CLEANUP_FUNC (XfcePanelPlugin, g_object_unref)
+#endif
 G_DECLARE_FINAL_TYPE (XfceMixerPlugin, xfce_mixer_plugin, XFCE, MIXER_PLUGIN, XfcePanelPlugin)
 
 void  xfce_mixer_plugin_register_type (XfcePanelTypeModule *type_module);